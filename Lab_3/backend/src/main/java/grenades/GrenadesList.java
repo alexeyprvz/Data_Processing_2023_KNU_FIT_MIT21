df6c3abdc6e5caf2bd60319fa840bab6b2cc12c7
alexeyprvz <alexeyprvz@knu.ua>
2023-02-27 03:11:35 +0200

    add content to the page and fix design

--- Lab_3/backend/src/main/java/grenades/GrenadesList.java
+++ Lab_3/backend/src/main/java/grenades/GrenadesList.java
@@ -10,9 +10,12 @@ public class GrenadesList {
     private List<Entity> grenadesList = new ArrayList<>();
 
     public GrenadesList(){
-        this.grenadesList.add(new Entity(0, "Something1", "1", "Smtth1", 11));
-        this.grenadesList.add(new Entity(1, "Something2", "1", "Smtth2", 12));
-        this.grenadesList.add(new Entity(2, "Something3", "1", "Smtth3", 13));
+        this.grenadesList.add(new Entity(0, "РГ-42", "1", "ручна, осколкова, наступальної дії", 420));
+        this.grenadesList.add(new Entity(1, "Ф-1", "2", "ручна, дистанційної дії", 60));
+        this.grenadesList.add(new Entity(2, "М-61", "3", "ручна, оборонна", 450));
+        this.grenadesList.add(new Entity(3, "M67", "4", "ручна, наступальна", 397));
+        this.grenadesList.add(new Entity(4, "РГД-5", "5", "протипіхотна, уламкова, ручна, дистанційної дії, наступальна", 310));
+        this.grenadesList.add(new Entity(5, "AN/M8", "6", "ручна, димова, отруйна", 680));
     }
 
     public List<Entity> getGrenadesList() {
